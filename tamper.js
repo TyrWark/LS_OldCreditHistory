7b7ff51c4fd701a1855673c023c2609f92af010f
TyrWark <167569189+TyrWark@users.noreply.github.com>
2026-01-09 13:44:55 -0600

    Add UserScript

---
+++ tamper.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Old Credits
+// @namespace    http://tampermonkey.net/
+// @version      2026-01-07
+// @description  try to take over the world!
+// @author       You
+// @match        https://us.merchantos.com/?name=customer.views.customer&form_name=view&id=*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @require      https://raw.githubusercontent.com/TyrWark/LS_OldCreditHistory/refs/heads/main/include.js
+// ==/UserScript==
+
